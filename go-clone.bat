dda830e322613b67be890d5a356e6b8515548a52
ccyzf <506439804@qq.com>
2024-03-24 14:43:42 +0800

    Add files via upload

---
+++ go-clone.bat
@@ -0,0 +1,4 @@
+runtime\python.exe webui.py
+CALL D:\anaconda3\Scripts\activate.bat  D:\anaconda3\envs\wav2lip\
+python wav2lip2former.py --input_img=C:\Users\yzf\Desktop\ALEX/3-ALEX-face.mp4 --input_wav=D:\yzf\SR/talk-head\Wav2Lip-former/input.wav
+pause
